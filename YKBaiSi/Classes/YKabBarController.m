eec0d1f02dd454c30b6c9accd33687288b7483b8
xiaofan <zhyks@zhyk.local>
2016-06-24 23:38:47 +0800

    添加 TabBar 中间的发布按钮

--- YKBaiSi/Classes/YKabBarController.m
+++ YKBaiSi/Classes/YKabBarController.m
@@ -8,11 +8,31 @@
 
 #import "YKabBarController.h"
 
+
+#define TabBar_Width 
+#define TabBar_Height
+
 @interface YKabBarController ()
+
+@property (nonatomic, strong) UIButton *publishButton;      // 中间的发布按钮
 
 @end
 
 @implementation YKabBarController
+
+/***  懒加载发布按钮 ***/
+- (UIButton *)publishButton {
+    if (!_publishButton) {
+        _publishButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        _publishButton.backgroundColor = YKRandomColor;
+        [_publishButton setImage:[UIImage imageNamed:@"tabBar_publish_icon"] forState:UIControlStateNormal];
+        [_publishButton setImage:[UIImage imageNamed:@"tabBar_publish_click_icon"] forState:UIControlStateHighlighted];
+        _publishButton.frame = CGRectMake(0, 0, self.tabBar.frame.size.width / 5, self.tabBar.frame.size.height);
+        _publishButton.center = CGPointMake(self.tabBar.frame.size.width * 0.5, self.tabBar.frame.size.height * 0.5);
+        [_publishButton addTarget:self action:@selector(publishClick) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _publishButton;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -35,10 +55,21 @@
     /**** 添加子控制器 ****/
     [self setupOneChildViewController:[[UITableViewController alloc] init] title:@"精华" image:@"tabBar_essence_icon" selectedImage:@"tabBar_essence_click_icon"];
     [self setupOneChildViewController:[[UITableViewController alloc] init] title:@"新帖" image:@"tabBar_new_icon" selectedImage:@"tabBar_new_click_icon"];
+    
+    // 中间按钮的占位 view
+    [self setupOneChildViewController:[[UIViewController alloc] init] title:nil image:nil selectedImage:nil];
+    
     [self setupOneChildViewController:[[UIViewController alloc] init] title:@"关注" image:@"tabBar_friendTrends_icon" selectedImage:@"tabBar_friendTrends_click_icon"];
     [self setupOneChildViewController:[[UITableViewController alloc] init] title:@"我" image:@"tabBar_me_icon" selectedImage:@"tabBar_me_click_icon"];
     
     
+}
+
+// 在 viewWillAppear 方法中添加发布按钮，因为这样可以在 tabBarButton 上
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self.tabBar addSubview:self.publishButton];
 }
 
 #pragma mark - 初始化子控制器
@@ -47,13 +78,17 @@
     
     vc.view.backgroundColor = YKRandomColor;
     vc.tabBarItem.title = title;
-    vc.tabBarItem.image = [UIImage imageNamed:image];
-    vc.tabBarItem.selectedImage = [UIImage imageNamed:selectedImage];
+    if (image.length) {
+        vc.tabBarItem.image = [UIImage imageNamed:image];
+        vc.tabBarItem.selectedImage = [UIImage imageNamed:selectedImage];
+    }
     [self addChildViewController:vc];
     
 }
 
-
+- (void)publishClick {
+    YKLogFunc;
+}
 
 
 
