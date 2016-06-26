1fa791c8e771f849ff8e5199b4d8fe3578591485
xiaofan <zhyks@zhyk.local>
2016-06-26 12:43:12 +0800

    设置登录注册界面 TextField 属性

--- XFBaisi/Classes/Other-其他/View/XFLoginRegisterTextField.m
+++ XFBaisi/Classes/Other-其他/View/XFLoginRegisterTextField.m
@@ -7,15 +7,41 @@
 //
 
 #import "XFLoginRegisterTextField.h"
+//#import <objc/runtime.h>
 
 @implementation XFLoginRegisterTextField
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+
+static NSString * const XFPlaceholderColorKey = @"_placeholderLabel.textColor";
+
+- (void)awakeFromNib {
+    // 设置光标颜色
+    self.tintColor = [UIColor whiteColor];
+    
+    // 利用运行时查看所有属性
+    //unsigned int count;
+    //Ivar *ivarList = class_copyIvarList([UITextField class], &count);
+    //for (int i = 0; i < count; i++) {
+        //Ivar ivar = ivarList[i];
+        //XFLog(@"%s", ivar_getName(ivar));
+    //}
+    //free(ivarList);
+    
+    [self setValue:[UIColor grayColor] forKeyPath:XFPlaceholderColorKey];
 }
-*/
+
+#pragma mark - 重写
+//- (void)drawPlaceholderInRect:(CGRect)rect {
+    //// 设置占位文字颜色
+    //NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
+    //attributes[NSForegroundColorAttributeName] = [UIColor whiteColor];
+    //attributes[NSFontAttributeName] = self.font;
+    
+    //CGPoint placeholderPoint = CGPointMake(0, (rect.size.height - self.font.lineHeight) * 0.5);
+    //[self.placeholder drawAtPoint:placeholderPoint withAttributes:attributes];
+    
+//}
+
+
 
 @end
