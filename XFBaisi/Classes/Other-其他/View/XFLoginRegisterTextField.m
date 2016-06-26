70ab6e2c30823fec9832f1cf57692177656c84ed
xiaofan <zhyks@zhyk.local>
2016-06-26 15:03:42 +0800

    UITextFieldDelegate 方法监测UITextField状态变化

--- XFBaisi/Classes/Other-其他/View/XFLoginRegisterTextField.m
+++ XFBaisi/Classes/Other-其他/View/XFLoginRegisterTextField.m
@@ -7,40 +7,46 @@
 //
 
 #import "XFLoginRegisterTextField.h"
-//#import <objc/runtime.h>
+
+
+
+@interface XFLoginRegisterTextField ()<UITextFieldDelegate>
+
+@end
 
 @implementation XFLoginRegisterTextField
 
-
 static NSString * const XFPlaceholderColorKey = @"_placeholderLabel.textColor";
+
 
 - (void)awakeFromNib {
     // 设置光标颜色
     self.tintColor = [UIColor whiteColor];
     
-    // 利用运行时查看所有属性
-    //unsigned int count;
-    //Ivar *ivarList = class_copyIvarList([UITextField class], &count);
-    //for (int i = 0; i < count; i++) {
-        //Ivar ivar = ivarList[i];
-        //XFLog(@"%s", ivar_getName(ivar));
-    //}
-    //free(ivarList);
+    // 设置默认的占位文字颜色
+    [self setValue:[UIColor grayColor] forKeyPath:XFPlaceholderColorKey];
     
+    self.delegate = self;
+}
+
+#pragma mark - <UITextFieldDelegate>
+
+/**
+ *  TextField 开始编辑
+ */
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    XFLogFunc;
+    [self setValue:[UIColor whiteColor] forKeyPath:XFPlaceholderColorKey];
+}
+
+/**
+ *  TextField 结束编辑
+ */
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    XFLogFunc;
     [self setValue:[UIColor grayColor] forKeyPath:XFPlaceholderColorKey];
 }
 
-#pragma mark - 重写
-//- (void)drawPlaceholderInRect:(CGRect)rect {
-    //// 设置占位文字颜色
-    //NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
-    //attributes[NSForegroundColorAttributeName] = [UIColor whiteColor];
-    //attributes[NSFontAttributeName] = self.font;
-    
-    //CGPoint placeholderPoint = CGPointMake(0, (rect.size.height - self.font.lineHeight) * 0.5);
-    //[self.placeholder drawAtPoint:placeholderPoint withAttributes:attributes];
-    
-//}
 
 
 
